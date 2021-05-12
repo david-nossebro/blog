485d74bb9d81d074110adb2644840caca444e47d
David Berg <david.nossebro@gmail.com>
2021-05-12 16:59:14 +0200

    Added iframe for Garmin MapShare (switch to own map later)

--- src/pages/position.tsx
+++ src/pages/position.tsx
@@ -13,14 +13,14 @@ const Position = ({ data }): JSX.Element => {
       <p>
         Här kan ni se hur det går för mig och var jag befinner mig just nu. Jag
         bär med mig en Garmin InReach vilket är en GPS/Nödsändare som man kan
-        skicka meddelanden via {" "}
+        skicka meddelanden via{" "}
         <a href="https://sv.wikipedia.org/wiki/Iridium_(satellittelefoni)">
-          Iridium 
-        </a>
-        {" "}
+          Iridium
+        </a>{" "}
         med. Med hjälp av den kommer jag kunna dela min position under hela min
         tur (förutsatt att jag har batteri kvar).
       </p>
+      <iframe src="https://share.garmin.com/KZHUA" frameborder="0" marginwidth="0" marginheight="0" width="810" height="760"></iframe>
     </Layout>
   )
 }
